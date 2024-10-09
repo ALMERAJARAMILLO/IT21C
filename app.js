6453c344d4ca27346c99e6094f0d5db8538332b9
ALMERAJARAMILLO <jaramilloalmera92@gmail.com>
2024-10-09 21:11:14 +1100

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//Input
+const person1 ={
+    name:"Almera S. Jaramillo",
+    age:19,
+    hobbies: "Reading Wattpad"
+}
+
+//Process and Output
+console.log("Name: "+person1.name);
+console.log("Age: "+person1.age);
+console.log("Hobbies:"+person1.hobbies);
