f2ea51065b530c9a5d160df48ee9da1b6b25aee0
ALMERAJARAMILLO <jaramilloalmera92@gmail.com>
2024-10-16 21:01:03 +1100

    Added changes in Chart.js

--- Charts/Chart.js
+++ Charts/Chart.js
@@ -58,6 +58,36 @@ class LineChart extends ChartCreator {
         });
     }
 }
+class BarChart extends ChartCreator {
+    constructor(dataUrl) {
+        super(dataUrl);
+        this.barCtx = document.getElementById('BarChart');
+    }
+
+    createCharts() {
+        this.createBarChart();
+    }
+    createBarChart() {
+        new Chart(this.barCtx, {
+            type: 'bar',
+            data: {
+                labels: this.chartData.labels,
+                datasets: [{
+                    label: '# of Students',
+                    data: this.chartData.data,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+}
 const lineChartCreator = new LineChart('Data.json');
 lineChartCreator.init();
 
